fb1edcad830b8f5a4f85480a2c395fc314674413
Bimo J.S <sukamulyaptk@gmail.com>
2026-07-18 12:11:04 +0700

    up

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,9 +5,9 @@ import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
 // Gunakan SERVICE_ROLE_KEY untuk bypass RLS (Row Level Security) karena ini dijalankan oleh Cron/Sistem
-const supabase = createServerClient(
+const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 );
 
 export async function GET(request: Request) {
